a1c59a2f145b4009034aaaeac570226107618229
cs617 <cs617@DESKTOP-RP3J9BK>
2017-09-14 11:18:00 +0900

    d

--- src/fsdfsd/ManagerTest.java
+++ src/fsdfsd/ManagerTest.java
@@ -1,5 +1,10 @@
 package fsdfsd;
 
 public class ManagerTest {
-
+	public static void main(String[] args){
+		Manager m = new Manager("Tom", "Seoul",1000000,12345,50000);
+		System.out.println(m);
+		m.test();
+				
+	}
 }
